24bf46aa1235622335e23abe34c82208a177bce2
Adithya Awati <1ds21ai001@dsce.edu.in>
2023-11-24 17:42:09 +0000

    update: about information

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <p>My name is <a href="https://www.linkedin.com/in/adithya-awati-87b7541a3/" target="_blank">Adithya Awati</a>, and I love training models, playing around with knobs of deep learning and <a href="https://github.com/p1utoze" target="_blank">contributing to open source</a>. I began my journey in 2019 during
+        <!-- <p>My name is <a href="https://www.linkedin.com/in/adithya-awati-87b7541a3/" target="_blank">Adithya Awati</a>, and I love training models, playing around with knobs of deep learning and <a href="https://github.com/p1utoze" target="_blank">contributing to open source</a>. I began my journey in 2019 during
             the COVID lockdown, starting with Python and C++ and creating small projects. Running into multiple errors on Windows (yes, the blue screen of death), I learned about Linux and shifted to my first distro- Mint. Since then I quickly gained a strong understanding of
             the system, using it majorly for all my development activities, and ultimately fell in love with OSS.
             <br><br>
@@ -15,6 +15,21 @@
             I am driven by my passion for deploying AI and actively seeking exciting
             full-time opportunities in the field. As a person who loves free stuff, I have spent a lot of time researching for open-source softwares to deploy and host my Deep learning models serving with FastAPI. I love challenges and debugging errors from the kernel level to the application level. <a href="mailto:aditya.n.awati@gmail.com">Feel free to reach out to me</a> through my inbox for networking and
             connections.
+        </p> -->
+        <p>
+            Hello there! I'm  <a href="https://www.linkedin.com/in/adithya-awati-87b7541a3/" target="_blank">Adithya Awati</a> , a deep learning aficionado and <a href="https://github.com/p1utoze" target="_blank">open-source software enthusiast</a>. My journey into the world of tech began in 2019, during the COVID lockdown. With Python and C++ as my trusty sidekicks, I embarked on a quest to create small projects. Along the way, I encountered numerous foes, including the infamous Windows Blue Screen of Death.
+
+This led me to discover the magical realm of Linux, and I quickly adopted my first distro - Mint. It was love at first sight! Since then, I've been using Linux for all my development escapades, and I've developed a deep understanding of the system. <br><br>
+
+I then dabbled in the art of web development, creating mini-projects with HTML, CSS, and JS. However, I soon realized that my heart belonged to the backend. As a script kiddie, I honed my skills in Python and C, and even managed to secure top 5 positions twice in Coding Contests at my university.
+
+My journey then took me to the world of desktop development, where I created a simple birthday reminder application using tkinter and QT. I also ventured into the realm of statistics and mathematics, building Time series models for agricultural yield prediction and securing 3rd place in the Diffuse AI hackathon.<br><br>
+
+Currently, I'm sharpening my skills in Natural Language Processing (NLP), developing advanced Recommender System models, and exploring Reinforcement Learning. I'm also delving into HMI plugins and SCADA systems. I've even dipped my toes into the fascinating world of Quantum Computing, learning about Qubits, Gates, Multi-qubit circuits, and building applications with diverse use cases.
+
+As a person who loves free stuff (who doesn't?), I've spent a lot of time hunting for open-source software to deploy and host my Deep Learning models. I love challenges and debugging errors, whether they're at the kernel level or the application level. <br><br>
+
+I'm currently on the lookout for exciting full-time opportunities in the field of AI. So, if you're looking for someone who can train models, play around with the knobs of deep learning, and debug errors while cracking a joke or two, <a href="mailto:aditya.n.awati@gmail.com">feel free to reach out!</a> Let's connect, network, and create something amazing together.
         </p>
     </div>
 </template>
