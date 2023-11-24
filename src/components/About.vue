38e3e6e1d4cf820af1f55c75e7913f199f6f6e28
Adithya Awati <1ds21ai001@dsce.edu.in>
2023-11-24 22:54:04 +0530

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,16 +1,17 @@
 <template>
     <div>
-        <p>My name is <a href="https://www.linkedin.com/in/praneeth-ravuri2500/" target="_blank">Adithya Awati</a>, and I love <a href="https://github.com/praneethravuri" target="_blank">developing websites and software engineering</a>. I began my journey in 2020 during
-            the COVID lockdown, starting with Python and creating small projects. I quickly gained a strong understanding of
-            the language and used it to build bots for scraping e-commerce websites.
+        <p>My name is <a href="https://www.linkedin.com/in/adithya-awati-87b7541a3/" target="_blank">Adithya Awati</a>, and I love training models, playing around with knobs of deep learning and <a href="https://github.com/p1utoze" target="_blank">contributing to open source</a>. I began my journey in 2019 during
+            the COVID lockdown, starting with Python and C++ and creating small projects. Running into multiple errors on Windows (yes, the blue screen of death), I learned about Linux and shifted to my first distro- Mint. Since then I quickly gained a strong understanding of
+            the system, using it majorly for all my development activities, and ultimately fell in love with OSS.
             <br>
-            I then ventured into building blogs on platforms like WordPress and blogger.com. This led to my appointment as a
-            web developer for the IEEE student body at my university. While working on enhancing the user interface of the
-            existing website, I faced challenges with CSS but persisted through them.
+            I then ventured into building mini-projects with basic HTML, CSS, and JS. Not really liking the Frontend dev side, I changed my gears to backend development spending my time as a script kiddie. I learned Python and C and showcased my skills
+            by securing the top 5 positions twice in Coding Contests at my university. I delved more into Desktop development from tkinter to QT building a simple birthday reminder application. 
             <br>
-            With <a href="https://drive.google.com/file/d/1AiUdUmqhrWz8wlmGVeOhDD2Pr5CBxW7C/view" target="_blank">two internships</a> in software and web development under my belt, I have now advanced to developing full-stack
-            applications with diverse use cases. I am driven by my passion for web development and actively seeking exciting
-            full-time opportunities in the field. <a href="mailto:aditya.n.awati@gmail.com">Feel free to reach out to me</a> through my inbox for networking and
+            I switched my next gear to explore statistics and mathematics like Linear algebra, the probability for machine learning, and building Time series models for agricultural yield prediction securing 3rd place in the Diffuse AI hacakthon. I began building applications over SQL databases like MariaDB, PostgresSQL and eventually built a Unified data portal for the Police Hackathon securing under top 10 position.
+            <br>
+            Currently, I am sharpening my skills in NLP, developing advanced Recommender System models, learning about Reinforcement learning, and delving into HMI plugins and SCADA systems. I have successfully completed a semester in Quantum Computing learning about Qubits, Gates, Multi-qubit circuits, and building
+            applications with diverse use cases. I am driven by my passion for deploying AI and actively seeking exciting
+            full-time opportunities in the field. As a person who loves free stuff, I have spent a lot of time researching for open-source softwares to deploy and host my Deep learning models serving with FastAPI. I love challenges and debugging errors from the kernel level to the application level. <a href="mailto:aditya.n.awati@gmail.com">Feel free to reach out to me</a> through my inbox for networking and
             connections.
         </p>
     </div>
